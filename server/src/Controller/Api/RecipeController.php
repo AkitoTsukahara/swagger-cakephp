0abbc8bc02652511ff4dcbed19678aad31cb7fa2
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-01-22 19:58:44 +0900

    recipe API ドキュメント更新

--- server/src/Controller/Api/RecipeController.php
+++ server/src/Controller/Api/RecipeController.php
@@ -21,6 +21,73 @@ class RecipeController extends AppController
         $this->loadComponent('RequestHandler');
     }
 
+    /**
+     * Index method
+     *
+     * @OA\Get(
+     *   path="/api/recipe/index.json",
+     *   tags={"Recipe"},
+     *   summary="レシピをすべて取得する",
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="recipes",
+     *         type="array",
+     *         description="レシピ一覧",
+     *         @OA\Items(
+     *           @OA\Property(
+     *             property="id",
+     *             type="string",
+     *             description="レシピID",
+     *           ),
+     *           @OA\Property(
+     *             property="title",
+     *             type="string",
+     *             description="レシピ名",
+     *           ),
+     *           @OA\Property(
+     *             property="description",
+     *             type="string",
+     *             description="レシピ内容",
+     *           ),
+     *         ),
+     *         example={
+     *           {
+     *             "id"="c366f5be-360b-45cc-8282-65c80e434f72",
+     *             "title"="ハンバーグ",
+     *             "description"="ジューシーでご飯がすすむ定番のハンバーグレシピです。ソースの材料もケチャップ、ウスターソース、醤油の３つだけとシンプル。",
+     *           },
+     *           {
+     *             "id"="93d5ef90-be4d-4179-9311-e39bddc26427",
+     *             "title"="親子丼",
+     *             "description"="ふんわりとろっとろな半熟仕立ての卵とだしの相性が抜群！ご飯にからんで食欲そそる、手軽なのに食べごたえもある満足度高めな一品です。",
+     *           },
+     *         },
+     *       ),
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response="default",
+     *     description="Unexpected Error",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="エラーメッセージ",
+     *       ),
+     *      example={
+     *        "message"="予期しないエラーです"
+     *      },
+     *     ),
+     *   ),
+     * )
+     *
+     * @return void
+     */
     public function index(): void
     {
         $recipes = $this->Recipes->find('all');
@@ -29,6 +96,73 @@ class RecipeController extends AppController
         $this->viewBuilder()->setOption('serialize', ['recipes']);
     }
 
+    /**
+     * View method
+     *
+     * @OA\Get(
+     *   path="/api/recipe/view/{id}.json",
+     *   tags={"Recipe"},
+     *   summary="レシピを参照する",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     description="レシピID",
+     *     @OA\Schema(type="string"),
+     *     example="c366f5be-360b-45cc-8282-65c80e434f72"
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="recipes",
+     *         type="object",
+     *         description="レシピ",
+     *         @OA\Property(
+     *           property="id",
+     *           type="string",
+     *           description="レシピID",
+     *         ),
+     *         @OA\Property(
+     *           property="title",
+     *           type="string",
+     *           description="レシピ名",
+     *         ),
+     *         @OA\Property(
+     *           property="description",
+     *           type="string",
+     *           description="レシピ内容",
+     *         ),
+     *         example={
+     *           "id"="c366f5be-360b-45cc-8282-65c80e434f72",
+     *           "title"="ハンバーグ",
+     *           "description"="ジューシーでご飯がすすむ定番のハンバーグレシピです。ソースの材料もケチャップ、ウスターソース、醤油の３つだけとシンプル。",
+     *         },
+     *       ),
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response="default",
+     *     description="Unexpected Error",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="エラーメッセージ",
+     *       ),
+     *      example={
+     *        "message"="予期しないエラーです"
+     *      },
+     *     ),
+     *   ),
+     * )
+     *
+     * @param string $id id.
+     * @return void
+     */
     public function view(string $id): void
     {
         $recipe = $this->Recipes->get($id);
@@ -36,6 +170,74 @@ class RecipeController extends AppController
         $this->viewBuilder()->setOption('serialize', ['recipe']);
     }
 
+    /**
+     * Add method
+     *
+     * @OA\Post(
+     *   path="/api/recipe/add.json",
+     *   tags={"Recipe"},
+     *   summary="レシピを登録する",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(
+     *       type="object",
+     *       required={"title","description"},
+     *       @OA\Property(
+     *         property="title",
+     *         type="string",
+     *         description="レシピ名",
+     *       ),
+     *       @OA\Property(
+     *         property="description",
+     *         type="string",
+     *         description="レシピ内容",
+     *       ),
+     *       example={
+     *         "title"="ハンバーグ",
+     *         "description"="ジューシーでご飯がすすむ定番のハンバーグレシピです。ソースの材料もケチャップ、ウスターソース、醤油の３つだけとシンプル。",
+     *       },
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="recipe",
+     *         type="object",
+     *         description="レシピ",
+     *         @OA\Property(
+     *           property="id",
+     *           type="string",
+     *           description="ID",
+     *         ),
+     *         example={
+     *           "id"="c366f5be-360b-45cc-8282-65c80e434f72",
+     *         },
+     *       ),
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response="default",
+     *     description="Unexpected Error",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="エラーメッセージ",
+     *       ),
+     *      example={
+     *        "message"="予期しないエラーです"
+     *      },
+     *     ),
+     *   ),
+     * )
+     *
+     * @return void
+     * @throws \App\Exception\ApplicationException
+     */
     public function add(): void
     {
         $this->request->allowMethod(['post', 'put']);
@@ -52,6 +254,83 @@ class RecipeController extends AppController
         $this->viewBuilder()->setOption('serialize', ['recipe', 'message']);
     }
 
+    /**
+     * Edit method
+     *
+     * @OA\Put(
+     *   path="/api/recipe/edit/{id}.json",
+     *   tags={"Recipe"},
+     *   summary="レシピを更新する",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     description="レシピID",
+     *     @OA\Schema(type="string"),
+     *     example="c366f5be-360b-45cc-8282-65c80e434f72"
+     *   ),
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(
+     *       type="object",
+     *       required={"description"},
+     *       @OA\Property(
+     *         property="title",
+     *         type="string",
+     *         description="レシピ名",
+     *       ),
+     *       @OA\Property(
+     *         property="description",
+     *         type="string",
+     *         description="レシピ内容",
+     *       ),
+     *       example={
+     *         "title"="ハンバーグ",
+     *         "description"="ジューシーでご飯がすすむ定番のハンバーグレシピです。ソースの材料もケチャップ、ウスターソース、醤油の３つだけとシンプル。",
+     *       },
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="recipe",
+     *         type="object",
+     *         description="レシピ",
+     *         @OA\Property(
+     *           property="id",
+     *           type="string",
+     *           description="ID",
+     *         ),
+     *         example={
+     *           "id"="c366f5be-360b-45cc-8282-65c80e434f72",
+     *         },
+     *       ),
+     *     ),
+     *   ),
+     *   @OA\Response(
+     *     response="default",
+     *     description="Unexpected Error",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="エラーメッセージ",
+     *       ),
+     *      example={
+     *        "message"="予期しないエラーです"
+     *      },
+     *     ),
+     *   ),
+     * )
+     *
+     * @param string $id id
+     * @return void
+     * @throws \App\Exception\ApplicationException
+     */
     public function edit(string $id): void
     {
         $this->request->allowMethod(['patch', 'post', 'put']);
@@ -69,6 +348,46 @@ class RecipeController extends AppController
         $this->viewBuilder()->setOption('serialize', ['recipe', 'message']);
     }
 
+    /**
+     * Delete method
+     *
+     * @OA\Delete(
+     *   path="/api/recipe/delete/{id}.json",
+     *   tags={"Recipe"},
+     *   summary="レシピを削除する",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     description="レシピID",
+     *     @OA\Schema(type="string"),
+     *     example="c366f5be-360b-45cc-8282-65c80e434f72"
+     *   ),
+     *   @OA\Response(
+     *     response=204,
+     *     description="No Content",
+     *   ),
+     *   @OA\Response(
+     *     response="default",
+     *     description="Unexpected Error",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="エラーメッセージ",
+     *       ),
+     *      example={
+     *        "message"="予期しないエラーです"
+     *      },
+     *     ),
+     *   ),
+     * )
+     *
+     * @param string $id id
+     * @return void
+     * @throws \App\Exception\ApplicationException
+     */
     public function delete(string $id): void
     {
         $this->request->allowMethod(['delete']);
@@ -81,95 +400,4 @@ class RecipeController extends AppController
         $this->viewBuilder()->setOption('serialize', ['message']);
     }
 
-//    /**
-//     * Index method
-//     *
-//     * @return \Cake\Http\Response|null|void Renders view
-//     */
-//    public function index()
-//    {
-//        $recipe = $this->paginate($this->Recipe);
-//
-//        $this->set(compact('recipe'));
-//    }
-//
-//    /**
-//     * View method
-//     *
-//     * @param string|null $id Recipe id.
-//     * @return \Cake\Http\Response|null|void Renders view
-//     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-//     */
-//    public function view($id = null)
-//    {
-//        $recipe = $this->Recipe->get($id, [
-//            'contain' => [],
-//        ]);
-//
-//        $this->set(compact('recipe'));
-//    }
-//
-//    /**
-//     * Add method
-//     *
-//     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-//     */
-//    public function add()
-//    {
-//        $recipe = $this->Recipe->newEmptyEntity();
-//        if ($this->request->is('post')) {
-//            $recipe = $this->Recipe->patchEntity($recipe, $this->request->getData());
-//            if ($this->Recipe->save($recipe)) {
-//                $this->Flash->success(__('The recipe has been saved.'));
-//
-//                return $this->redirect(['action' => 'index']);
-//            }
-//            $this->Flash->error(__('The recipe could not be saved. Please, try again.'));
-//        }
-//        $this->set(compact('recipe'));
-//    }
-//
-//    /**
-//     * Edit method
-//     *
-//     * @param string|null $id Recipe id.
-//     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-//     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-//     */
-//    public function edit($id = null)
-//    {
-//        $recipe = $this->Recipe->get($id, [
-//            'contain' => [],
-//        ]);
-//        if ($this->request->is(['patch', 'post', 'put'])) {
-//            $recipe = $this->Recipe->patchEntity($recipe, $this->request->getData());
-//            if ($this->Recipe->save($recipe)) {
-//                $this->Flash->success(__('The recipe has been saved.'));
-//
-//                return $this->redirect(['action' => 'index']);
-//            }
-//            $this->Flash->error(__('The recipe could not be saved. Please, try again.'));
-//        }
-//        $this->set(compact('recipe'));
-//    }
-//
-//    /**
-//     * Delete method
-//     *
-//     * @param string|null $id Recipe id.
-//     * @return \Cake\Http\Response|null|void Redirects to index.
-//     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-//     */
-//    public function delete($id = null)
-//    {
-//        $this->request->allowMethod(['post', 'delete']);
-//        $recipe = $this->Recipe->get($id);
-//        if ($this->Recipe->delete($recipe)) {
-//            $this->Flash->success(__('The recipe has been deleted.'));
-//        } else {
-//            $this->Flash->error(__('The recipe could not be deleted. Please, try again.'));
-//        }
-//
-//        return $this->redirect(['action' => 'index']);
-//    }
 }
